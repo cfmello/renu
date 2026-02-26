45b2314fa68e8c2785986531eb3fb4326af12f58
Cristhiano Mello <maguinho@gmail.com>
2026-02-26 14:52:56 -0300

    identação schemas corrigido

--- schemas.py
+++ schemas.py
@@ -17,18 +17,18 @@ class ItemCreate(BaseModel):
     categoria_id: int
 
     @model_validator(mode='after')
-        def validar_campos(self) -> 'ItemCreate':
-            agora = datetime.now(timezone.utc)
-            
-            if self.tipo_controle == "PRAZO":
-                if self.prazo_dias is None:
-                    raise ValueError("Para o tipo PRAZO, informe os dias.")
-            
-            elif self.tipo_controle == "VALIDADE":
-                if self.data_validade_fixa is None:
-                    raise ValueError("Para o tipo VALIDADE, informe a data.")
-                # Impede datas retroativas (com margem de erro de 1 min)
-                if self.data_validade_fixa.replace(tzinfo=timezone.utc) < agora:
-                    raise ValueError("A data de validade não pode estar no passado.")
-            
-            return self
+    def validar_campos(self) -> 'ItemCreate':
+        agora = datetime.now(timezone.utc)
+        
+        if self.tipo_controle == "PRAZO":
+            if self.prazo_dias is None:
+                raise ValueError("Para o tipo PRAZO, informe os dias.")
+        
+        elif self.tipo_controle == "VALIDADE":
+            if self.data_validade_fixa is None:
+                raise ValueError("Para o tipo VALIDADE, informe a data.")
+            # Impede datas retroativas (com margem de erro de 1 min)
+            if self.data_validade_fixa.replace(tzinfo=timezone.utc) < agora:
+                raise ValueError("A data de validade não pode estar no passado.")
+        
+        return self
